51dba63109ed65f95f0f77daca753e77f5e17e6f
Paulo Barata <pbara7a@gmail.com>
2022-07-23 12:08:38 +0100

    User domain.

---
+++ src/domain/user.js
@@ -0,0 +1,135 @@
+import dbClient from "../utils/dbClient.js";
+import bcrypt from "bcrypt";
+
+export default class User {
+  static fromDb(user) {
+    return new User(
+      user.id,
+      user.profile.firstName,
+      user.profile.lastName,
+      user.email,
+      user.password
+    );
+  }
+
+  static async fromJson({ first_name, last_name, email, password }) {
+    let passwordHash;
+    if (password) {
+      passwordHash = await bcrypt.hash(password, 8);
+    }
+
+    return new User(null, first_name, last_name, email, passwordHash);
+  }
+
+  constructor(id, firstName, lastName, email, passwordHash) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.passwordHash = passwordHash;
+  }
+
+  toJSON() {
+    return {
+      user: {
+        id: this.id,
+        first_name: this.firstName,
+        last_name: this.lastName,
+        email: this.email,
+      },
+    };
+  }
+
+  async save() {
+    const createdUser = await dbClient.user.create({
+      data: {
+        email: this.email,
+        password: this.passwordHash,
+        profile: {
+          create: {
+            firstName: this.firstName,
+            lastName: this.lastName,
+          },
+        },
+      },
+      include: {
+        profile: true,
+      },
+    });
+
+    return User.fromDb(createdUser);
+  }
+
+  async update() {
+    const updatedUser = await dbClient.user.update({
+      where: {
+        id: this.id,
+      },
+      data: {
+        email: this.email,
+        password: this.passwordHash,
+        profile: {
+          update: {
+            firstName: this.firstName,
+            lastName: this.lastName,
+          },
+        },
+      },
+      include: {
+        profile: true,
+      },
+    });
+
+    return User.fromDb(updatedUser);
+  }
+
+  static async findByEmail(email) {
+    return User._findByUnique("email", email);
+  }
+
+  static async findById(id) {
+    return User._findByUnique("id", id);
+  }
+
+  static async findAll({ whereData }) {
+    return User._findMany({ whereData });
+  }
+
+  static async _findByUnique(key, value) {
+    const foundUser = await dbClient.user.findUnique({
+      where: {
+        [key]: value,
+      },
+      include: {
+        profile: true,
+      },
+    });
+
+    if (foundUser) {
+      return User.fromDb(foundUser);
+    }
+
+    return null;
+  }
+
+  static async _findMany({ key, value, whereData }) {
+    const query = {
+      where: { ...whereData },
+      include: {
+        profile: true,
+      },
+    };
+
+    if (key !== undefined && value !== undefined) {
+      query.where = {
+        profile: {
+          [key]: value,
+        },
+      };
+    }
+
+    const foundUsers = await dbClient.user.findMany(query);
+
+    return foundUsers.map((user) => User.fromDb(user));
+  }
+}
