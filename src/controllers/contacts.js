0ac01d6f56c7cae5bbd2dd45fdeda21458db1529
Paulo Barata <pbara7a@gmail.com>
2022-07-24 17:00:24 +0100

    Controller to get contacts of a user.

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,17 @@
-const User = require("../domain/user");
 const Contact = require("../domain/contact");
 const { sendDataResponse, sendMessageResponse } = require("../utils/responses");
+
+const contacts = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const foundContacts = await Contact.findAllFromUser(Number(userId));
+
+    return sendDataResponse(res, 200, foundContacts);
+  } catch (e) {
+    return sendMessageResponse(res, 500, e.message);
+  }
+};
 
 const createContact = async (req, res) => {
   const contactToCreate = await Contact.fromJson(req.body);
@@ -14,4 +25,4 @@ const createContact = async (req, res) => {
   }
 };
 
-module.exports = { createContact };
+module.exports = { createContact, contacts };
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const contactsController = require("../controllers/contacts");
 
+router.get("/:userId", contactsController.contacts);
 router.post("/", contactsController.createContact);
 
 module.exports = router;
